f32a4ea1bf0e49d4e3d47d739cd7ce88e933a7a1
George G <kreach3r@users.noreply.github.com>
2016-10-07 17:45:45 +0200

    Rewrite Android.mk

--- Android.mk
+++ Android.mk
@@ -11,29 +11,35 @@ LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
 LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
 LOCAL_RESOURCE_DIR += frameworks/support/design/res
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+# Include KatsunaCommon into this app
+LOCAL_REQUIRED_MODULES := KatsunaCommon
+LOCAL_STATIC_JAVA_LIBRARIES := KatsunaCommon
+
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
-LOCAL_STATIC_JAVA_AAR_LIBRARIES += roundedimageview
+
+# Include the specified aar, DEFINED in KatsunaCommon's Android.mk!
+LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --generate-dependencies
+# TODO: Don't know if actually needed
+LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.common
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
 LOCAL_AAPT_FLAGS += --extra-packages android.support.design
-LOCAL_AAPT_FLAGS += --extra-packages picasso
-LOCAL_AAPT_FLAGS += --extra-packages jodatime
-LOCAL_AAPT_FLAGS += --extra-packages roundedimageview
-
+# This is to include the aar's RESOURCES into this app
+# Notice the full packagename
+LOCAL_AAPT_FLAGS += --extra-packages com.makeramen.roundedimageview
 
 LOCAL_PACKAGE_NAME := KatsunaCalls
 LOCAL_CERTIFICATE := platform
 #LOCAL_PROGUARD_FLAG_FILES := app/proguard-rules.pro
 
 LOCAL_PROGUARD_ENABLED := disabled
-
-include packages/apps/KatsunaCommon/common.mk
 
 include $(BUILD_PACKAGE)
 

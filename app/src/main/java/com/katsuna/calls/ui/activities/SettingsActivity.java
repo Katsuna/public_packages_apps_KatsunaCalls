0f5fa03bc56108e9909661e4cae82ce2cad50e9e
alkis <a.kafkis@gmail.com>
2018-02-18 11:56:29 +0200

    App settings redesign v2 - part1.

--- app/src/main/java/com/katsuna/calls/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/katsuna/calls/ui/activities/SettingsActivity.java
@@ -1,24 +1,15 @@
 package com.katsuna.calls.ui.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ScrollView;
 
 import com.katsuna.calls.R;
 import com.katsuna.commons.entities.ColorProfile;
 import com.katsuna.commons.entities.SizeProfile;
-import com.katsuna.commons.ui.SettingsKatsunaActivity;
-import com.katsuna.commons.utils.ColorAdjuster;
-import com.katsuna.commons.utils.ProfileReader;
+import com.katsuna.commons.ui.SettingsActivityBase;
 import com.katsuna.commons.utils.SizeAdjuster;
-import com.katsuna.commons.utils.ViewUtils;
 
-public class SettingsActivity extends SettingsKatsunaActivity {
-
-    private Button mDeleteButton;
+public class SettingsActivity extends SettingsActivityBase {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,40 +24,22 @@ public class SettingsActivity extends SettingsKatsunaActivity {
         super.onResume();
 
         applyProfiles();
-        loadProfiles();
-        applyColorProfile(mUserProfileContainer.getColorProfile());
     }
 
     @Override
     protected void applyColorProfile(ColorProfile colorProfile) {
-        ColorProfile profile = colorProfile;
-        if (colorProfile == ColorProfile.AUTO) {
-            if (mUserProfileContainer.hasKatsunaServices()) {
-                profile = ProfileReader.getUserProfileFromKatsunaServices(this).colorProfile;
-            } else {
-                profile = ColorProfile.COLOR_IMPAIREMENT;
-            }
-        }
-        ColorAdjuster.adjustButtons(this, profile, mDeleteButton, null);
+        // no op here
     }
 
     @Override
     protected void applySizeProfile(SizeProfile sizeProfile) {
-        ViewGroup topViewGroup = (ViewGroup) findViewById(android.R.id.content);
+        ViewGroup topViewGroup = findViewById(android.R.id.content);
         SizeAdjuster.applySizeProfile(this, topViewGroup, sizeProfile);
     }
 
-    private void initControls() {
+    protected void initControls() {
+        super.initControls();
         initToolbar();
-        initAppSettings();
-        mScrollViewContainer = (ScrollView) findViewById(R.id.scroll_view_container);
-
-        mDeleteButton = (Button) findViewById(R.id.deleteContacts);
-        mDeleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SettingsActivity.this, SelectCallsActivity.class));
-            }
-        });
+        mScrollViewContainer = findViewById(R.id.scroll_view_container);
     }
 }

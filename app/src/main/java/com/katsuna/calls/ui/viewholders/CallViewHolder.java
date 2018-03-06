8b01b0d033f6674819614fcebbb7db56248072aa
alkis <a.kafkis@gmail.com>
2018-03-06 20:10:41 +0200

    More actions for conversations activity.

--- app/src/main/java/com/katsuna/calls/ui/viewholders/CallViewHolder.java
+++ app/src/main/java/com/katsuna/calls/ui/viewholders/CallViewHolder.java
@@ -83,24 +83,6 @@ public class CallViewHolder extends CallBaseViewHolder {
             }
         }
 
-        //
-/*        SizeProfile sizeProfile = mUserProfileContainer.getOpticalSizeProfile();
-        int size = itemView.getResources()
-                .getDimensionPixelSize(R.dimen.common_icon_h_intermediate);
-        if (sizeProfile == SizeProfile.SIMPLE) {
-            size = itemView.getResources().getDimensionPixelSize(R.dimen.common_icon_h_simple);
-        }
-
-        if (call.getType() == CallLog.Calls.MISSED_TYPE) {
-            size = itemView.getResources()
-                    .getDimensionPixelSize(R.dimen.call_type_circle_size);
-        }
-
-        ViewGroup.LayoutParams layoutParams = mCallTypeImage.getLayoutParams();
-        layoutParams.height = size;
-        layoutParams.width = size;
-        mCallTypeImage.setLayoutParams(layoutParams);*/
-
         adjustProfile();
     }
 }

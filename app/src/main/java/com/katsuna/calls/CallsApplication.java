da733456f59beea6c5fca937566fdecb5584114b
alkis <a.kafkis@gmail.com>
2018-03-26 20:56:14 +0300

    missing google services handling for firebase init.

--- app/src/main/java/com/katsuna/calls/CallsApplication.java
+++ app/src/main/java/com/katsuna/calls/CallsApplication.java
@@ -1,17 +1,24 @@
 package com.katsuna.calls;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.google.firebase.crash.FirebaseCrash;
 
 public class CallsApplication extends Application {
 
+    private static final String TAG = "CallsApplication";
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // disable firebase crash collection for debug
-        FirebaseCrash.setCrashCollectionEnabled(!BuildConfig.DEBUG);
+        try {
+            // disable firebase crash collection for debug
+            FirebaseCrash.setCrashCollectionEnabled(!BuildConfig.DEBUG);
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage());
+        }
     }
 
 }
